74602b7fe0c733b5429d107ea5e6071872ba4d6e
Alexander <antidze.sasha@gmail.com>
2017-06-09 17:21:08 +0400

    adding button styling & fixing layout

--- category.php
+++ category.php
@@ -8,28 +8,6 @@ if(isset($_GET['category'])){
 	$get_cat_title = "SELECT cat_title FROM categories WHERE cat_id = $post_category";
 	$get_cat_title_sql = mysqli_query($connection, $get_cat_title);
 	$cat_name = mysqli_fetch_assoc($get_cat_title_sql);
-				
-				
-				
-	$query = "SELECT * FROM posts WHERE post_category_id = $post_category AND post_status = 'published'";
-	$select_all_posts_query = mysqli_query($connection, $query);
-				
-	if(empty(mysqli_num_rows($select_all_posts_query))){
-		echo '<h1>No posts are available under this category</h1>';
-	}
-				
-	else{
-				
-	$row = mysqli_fetch_assoc($select_all_posts_query);
-
-	$post_title = $row['post_title'];
-	$post_id = $row['post_id'];
-	$post_author = $row['post_author'];
-	$post_date = $row['post_date'];
-	$post_image = $row['post_image'];
-	$post_content = substr($row['post_content'], 0, 150);
-					}
-	
 }
 ?>
  
@@ -51,10 +29,30 @@ if(isset($_GET['category'])){
         <div class="row">
 
             <!-- Blog Entries Column -->
-            <div class="col-md-7">
+            <div class="col-md-7 col-md-offset-2">
           
 
+	<?php
+	
+	$query = "SELECT * FROM posts WHERE post_category_id = $post_category AND post_status = 'published'";
+	$select_all_posts_query = mysqli_query($connection, $query);
+	
+	if(empty(mysqli_num_rows($select_all_posts_query))){
+		echo '<h1>No posts are available under this category</h1>';
+	}
+
+	else{
+
+		while($row = mysqli_fetch_assoc($select_all_posts_query)){
+
+		$post_title = $row['post_title'];
+		$post_id = $row['post_id'];
+		$post_author = $row['post_author'];
+		$post_date = $row['post_date'];
+		$post_image = $row['post_image'];
+		$post_content = substr($row['post_content'], 0, 150);
 							
+	?>
 
 	<h2 class="post_title text-center">
 	<a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title ?></a>
@@ -69,11 +67,12 @@ if(isset($_GET['category'])){
 	<div id="postContent"><?php echo $post_content ?></div>
 	<a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id; ?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 	<hr>
+	<?php  }  }?>
 						
 			
             </div>
 
-            <?php include 'includes/sidebar.php'; ?>
+            
 
         </div>
         <!-- /.row -->

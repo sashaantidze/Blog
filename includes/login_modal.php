8d487cad07f05374e747610fecdebece4da76273
Alexander <antidze.sasha@gmail.com>
2017-06-09 17:44:54 +0400

    adding login modal form

---
+++ includes/login_modal.php
@@ -0,0 +1,16 @@
+<div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
+    	  <div class="modal-dialog">
+				<div class="loginmodal-container">
+					<h1>Login to Your Account</h1><br>
+				  <form action="includes/login.php" method="post">
+					<input type="text" name="username" placeholder="Username">
+					<input type="password" name="password" placeholder="Password">
+					<input type="submit" name="login" class="login loginmodal-submit" value="Login">
+				  </form>
+					
+				  <div class="login-help">
+					<a href="registration.php">Sign Up</a>
+				  </div>
+				</div>
+			</div>
+		  </div>
